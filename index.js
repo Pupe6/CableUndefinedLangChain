f85fcb6731d69a710ccc9a4648423f467301a5d2
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-10-23 21:29:09 +0300

    init dependencies and document loaders

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+// Import dotenv for api_keys and fs for loading files
 import { config } from 'dotenv'
+import fs from 'fs'
 config()
 
+// Prompt Template for custom prompt questions
 import { PromptTemplate } from 'langchain/prompts'
-import { OpenAI } from "langchain/llms/openai"
-import { LLMChain } from "langchain/chains"
 
-const model = new OpenAI({temperature: 0.1})
+// Document Loaders
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory"
+import { CSVLoader } from "langchain/document_loaders/fs/csv"
+import { TextLoader } from 'langchain/document_loaders/fs/text'
+
+// For splitting the text into chunks 
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+
+// LLM Chains and Model
+import { OpenAI } from "langchain/llms/openai"
+import { RetrievalQAChain } from "langchain/chains"
+
+// Vector database
+import { HNSWLib } from "langchain/vectorstores/hnswlib"
+
+// Embeddings for text
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"
+
+// Initialize document loaders
+const loader = new DirectoryLoader("./documents", {
+    ".csv": (path) => new CSVLoader(path),
+    ".xlsx": (path) => new CSVLoader(path),
+    ".txt": (path) => new TextLoader(path),
+})
+
+
 const template = "Tell me how to wire {micro_controller} to {embedded_module}"
 
 const prompt = new PromptTemplate({template, inputVariables: ['micro_controller', 'embedded_module']})
 
-const chain = new LLMChain({llm: model, prompt})
-
-const result = await chain.call({micro_controller: 'Arduino', embedded_module: 'PN532'})
-
-console.log(result)
