6e3deb5dc2f01e0b4948b3e8945a7eae0fd5f6aa
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-10-24 21:16:06 +0300

    text processing

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ import { HNSWLib } from "langchain/vectorstores/hnswlib"
 // Embeddings for text
 import { OpenAIEmbeddings } from "langchain/embeddings/openai"
 
+const VECTOR_STORE_PATH = "Components_info.index"
+
 // Initialize document loaders
 const loader = new DirectoryLoader("./documents", {
     ".csv": (path) => new CSVLoader(path),
@@ -36,8 +38,40 @@ console.log("Loading documents...")
 const documents = await loader.load()
 console.log("Loaded documents")
 
+// Normalize the documents for better preprocessing
+function normalizeDocuments(documents){
+    return documents.map((document) => {
+        if (typeof document.pageContent === 'string'){
+            return document.pageContent
+        }
+        else if (Array.isArray(document.pageContent)){
+            return document.pageContent.join('\n')
+        }
+    })
+}
 
 const template = "Tell me how to wire {micro_controller} to {embedded_module}"
 
 const prompt = new PromptTemplate({template, inputVariables: ['micro_controller', 'embedded_module']})
 
+export const main_function = async () => {
+    // Initialize the model
+    const model = new OpenAI({})
+    let vectorStore
+
+    // Check if the vector store exists
+    if (fs.existsSync(VECTOR_STORE_PATH)){
+        console.log("Loading an existing vector store...")
+        vectorStore = await HNSWLib.load(VECTOR_STORE_PATH, new OpenAIEmbeddings)
+        console.log("Loaded an existing vector store")
+    }
+    else{
+        console.log("Creating a new vector store...")
+
+        // For splitting the text into chunks
+        const textSplitter = new RecursiveCharacterTextSplitter({chunkSize: 1000, chunkOverlap: 100})
+        const normalized_docs = new normalizeDocuments(documents)
+        const splitted_docs = await textSplitter.createDocuments(normalized_docs)
+    }
+    
+}
