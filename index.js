b3d8e16e27b3f8f7fc383da423bbf2443e8bc43e
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-10-27 17:45:53 +0300

    not working checkpoint

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ import { TextLoader } from 'langchain/document_loaders/fs/text'
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 
 // LLM Chains and Model
-import { ChatOpenAI } from "langchain/chat_models/openai"
+import { OpenAI } from "langchain/llms/openai"
 import { ConversationChain } from "langchain/chains"
 
-// Prompt Templates and Buffer Memory
-import {ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder,
-} from "langchain/prompts";
+// Prompt Template
+import { PromptTemplate } from "langchain/prompts"
 
+// Buffer Memory
 import { BufferMemory } from "langchain/memory";
 
 // Vector database
@@ -32,7 +32,7 @@ const VECTOR_STORE_PATH = "Components_info.index"
 // This variables should be taken from the api
 const micro_controller = "ARDUINO"
 const embedded_module = "PN532 NFC Module"
-const question = `Tell me how to wire ${micro_controller} to ${embedded_module}`
+const query = `Tell me how to wire ${micro_controller} to ${embedded_module}`
 
 const question2 = "What are the components that we can connect wit ARDUINO"
 
@@ -64,8 +64,12 @@ function normalizeDocuments(documents){
 
 export const main_function = async () => {
     // Initialize the model
-    const chat = new ChatOpenAI({ temperature: 0.1 })
+    const model = new OpenAI({ temperature: 0.1 })
     let vectorStore
+
+    const prompt = PromptTemplate.fromTemplate(
+        "How to wire {micro_controller} to {embedded_component}?"
+    );
 
     // Check if the vector store exists
     if (fs.existsSync(VECTOR_STORE_PATH)){
@@ -88,34 +92,23 @@ export const main_function = async () => {
         console.log("New vector store created and saved")
     }
 
-    // Promts for the chat
-    const chatPrompt = ChatPromptTemplate.fromMessages([
-    SystemMessagePromptTemplate.fromTemplate(
-        "The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details how to wire different embedded components from its context. If the AI does not know the answer to a question, it truthfully says it does not know."
-    ),
-    new MessagesPlaceholder("history"),
-    HumanMessagePromptTemplate.fromTemplate("{input}"),
-    ]);
-
 
 
     // Initialize the chain
     console.log("Initializing the chain...")
     const chain = new ConversationChain({
         memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
-        prompt: chatPrompt,
-        llm: chat,
+        llm: model,
     });
 
     // Ask the question
-    console.log({question})
+    console.log({query})
     const response = await chain.call({
-        input: {
-            question: question,
-            vectorStore: vectorStore
-        },
+        micro_controller: "ARDUINO",
+        embedded_component: "PN532 NFC Module",
     });
-    console.log({response})
+
+    console.log(response)
 }
 
 // Run the main function
