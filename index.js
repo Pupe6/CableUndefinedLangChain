c4cabd8f7476ec8b2be3d54a19836f9d637edba4
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-10-27 20:30:12 +0300

    Conversation chain without retriever

--- index.js
+++ index.js
@@ -7,32 +7,29 @@ config()
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory"
 import { CSVLoader } from "langchain/document_loaders/fs/csv"
 import { TextLoader } from 'langchain/document_loaders/fs/text'
-
 // For splitting the text into chunks 
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 
 // LLM Chains and Model
-import { OpenAI } from "langchain/llms/openai"
-import { ConversationChain } from "langchain/chains"
+import { ChatOpenAI } from "langchain/chat_models/openai"
+import { ConversationChain} from "langchain/chains"
 
-// Prompt Template
-import { PromptTemplate } from "langchain/prompts"
+// Prompt Templates and Buffer Memory
+import {ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder,
+} from "langchain/prompts";
 
-// Buffer Memory
 import { BufferMemory } from "langchain/memory";
 
 // Vector database
 import { HNSWLib } from "langchain/vectorstores/hnswlib"
-
 // Embeddings for text
 import { OpenAIEmbeddings } from "langchain/embeddings/openai"
 
 const VECTOR_STORE_PATH = "Components_info.index"
-
 // This variables should be taken from the api
 const micro_controller = "ARDUINO"
 const embedded_module = "PN532 NFC Module"
-const query = `Tell me how to wire ${micro_controller} to ${embedded_module}`
+const question = `Tell me how to wire ${micro_controller} to ${embedded_module}`
 
 const question2 = "What are the components that we can connect wit ARDUINO"
 
@@ -43,12 +40,10 @@ const loader = new DirectoryLoader("./documents", {
     ".xlsx": (path) => new CSVLoader(path),
     ".txt": (path) => new TextLoader(path),
 })
-
 console.log("Loading documents...")
 // Load the documents
 const documents = await loader.load()
 console.log("Loaded documents")
-
 // Normalize the documents for better preprocessing
 function normalizeDocuments(documents){
     return documents.map((document) => {
@@ -64,12 +59,8 @@ function normalizeDocuments(documents){
 
 export const main_function = async () => {
     // Initialize the model
-    const model = new OpenAI({ temperature: 0.1 })
+    const chat = new ChatOpenAI({ temperature: 0.1, modelName: "gpt-3.5-turbo" })
     let vectorStore
-
-    const prompt = PromptTemplate.fromTemplate(
-        "How to wire {micro_controller} to {embedded_component}?"
-    );
 
     // Check if the vector store exists
     if (fs.existsSync(VECTOR_STORE_PATH)){
@@ -87,10 +78,18 @@ export const main_function = async () => {
 
         // Create a new vector store
         vectorStore = await HNSWLib.fromDocuments(splitted_docs, new OpenAIEmbeddings())
-
         await vectorStore.save(VECTOR_STORE_PATH)
         console.log("New vector store created and saved")
     }
+
+    // Promts for the chat
+    const chatPrompt = ChatPromptTemplate.fromMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+        "The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details how to wire different embedded components from its context. If the AI does not know the answer to a question, it truthfully says it does not know."
+    ),
+    new MessagesPlaceholder("history"),
+    HumanMessagePromptTemplate.fromTemplate("{input}"),
+    ]);
 
 
 
@@ -98,16 +97,16 @@ export const main_function = async () => {
     console.log("Initializing the chain...")
     const chain = new ConversationChain({
         memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
-        llm: model,
+        prompt: chatPrompt,
+        llm: chat,
+        retriever: vectorStore
     });
 
     // Ask the question
-    console.log({query})
+    console.log({question})
     const response = await chain.call({
-        micro_controller: "ARDUINO",
-        embedded_component: "PN532 NFC Module",
+        input: question 
     });
-
     console.log(response)
 }
 
