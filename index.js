0304925dace760f1f5633c71a13cf5f9371c6ce4
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-11-07 20:24:34 +0200

    basic logic for api fetch

--- index.js
+++ index.js
@@ -106,5 +106,48 @@ export const main_function = async (micro_controller, embedded_module) => {
     console.log({ res })
 }
 
+
+
+const sendHttpRequest = (method, url, data) => {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: data ? { 'Content-Type': 'application/json' } : {}
+  }).then(response => {
+    if (response.status >= 400) {
+      // !response.ok
+      return response.json().then(errResData => {
+        const error = new Error('Something went wrong!');
+        error.data = errResData;
+        throw error;
+      });
+    }
+    return response.json();
+  });
+};
+
+
+const getData = () => {
+  sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData => {
+    console.log(responseData);
+  });
+};
+
+const sendData = () => {
+  sendHttpRequest('POST', 'https://reqres.in/api/register', {
+    email: 'eve.holt@reqres.in'
+    // password: 'pistol'
+  })
+    .then(responseData => {
+      console.log(responseData);
+    })
+    .catch(err => {
+      console.log(err, err.data);
+    });
+};
+
+
+
+
 // Run the main function
 main_function("ESP32", "SEN18 Water Sensor")
